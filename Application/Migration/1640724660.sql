12ac98cca42f676cc313dd9cba388939707bdda8
Marcos Tirao <marcos.tirao@icloud.com>
2022-02-26 08:48:10 -0300

    Migration to Scotty

--- Application/Migration/1640724660.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE calendars (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    doctor_id UUID NOT NULL,
-    date TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
-);
-CREATE INDEX calendars_doctor_id_index ON calendars (doctor_id);
-ALTER TABLE calendars ADD CONSTRAINT calendars_ref_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctors (id) ON DELETE NO ACTION;
